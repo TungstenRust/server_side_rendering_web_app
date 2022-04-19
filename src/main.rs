895d4e050cbbc486eb816803e2d4406ac87fe4c3
Tungsten Rust <tungsten.rust@gmail.com>
2022-04-19 03:42:38 -0700

    handlebars used in html

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,45 @@
-use actix_files::{Files, NamedFile};
-use actix_web::{web, App, HttpServer, Result};
+use actix_files::Files;
+use actix_web::{web, App, HttpServer, HttpResponse};
+use handlebars::Handlebars;
+use serde_json::json;
 
-async fn index() ->Result<NamedFile>{
-    Ok(NamedFile::open("./static/index.html")?)
+async fn index(handlebar:web::Data<Handlebars<'_>>) ->HttpResponse{
+    let data = json!({
+        "project_name": "Certifications",
+        "certs": [
+            {
+                "name": "Professional",
+                "image_path":
+                "/static/image/cdp.png"
+            },
+            {
+                "name": "Expert",
+                "image_path":
+                "/static/image/cde.png"
+            },
+            {
+                "name": "Architect",
+                "image_path":
+                "/static/image/cda.png"
+            }
+        ]
+    });
+    let body = handlebar.render("index", &data).unwrap();
+    HttpResponse::Ok().body(body)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let mut handlebars = Handlebars::new();
+    handlebars
+        .register_templates_directory(".html", "./static/")
+        .unwrap();
+    let handlebars_ref = web::Data::new(handlebars);
+
     println!("Listening on port 8080");
-    HttpServer::new(||{
+    HttpServer::new(move ||{
         App::new()
+            .app_data(handlebars_ref.clone())
             .service(
                 Files::new("/static", "static")
                     .show_files_listing(),
